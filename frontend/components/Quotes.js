cd62f10172f3a0d8f201350516953029bcf8df00
Katy Kane <katytheartist@gmail.com>
2024-03-22 16:01:39 -0500

    final

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -4,34 +4,43 @@ import {
   setHighlightedQuote,
   toggleVisibility,
 } from '../state/quotesSlice'
+import {useGetQuotesQuery, useToggleFakeMutation, useDeleteQuoteMutation} from '../state/quotesApi'
 
 export default function Quotes() {
-  const quotes = [
-    {
-      id: 1,
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: true,
-    },
-    {
-      id: 2,
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
-    },
-    {
-      id: 3,
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
-    },
-  ]
+  const {data: quotes, isLoading: quotesLoading, isFetching: quotesRefreshing} = useGetQuotesQuery()
+  console.log(quotes)
+  const [toggleFake, {isLoading: quoteToggling}] = useToggleFakeMutation()
+  const [deleteQuote, {isLoading: quoteDeleting}] = useDeleteQuoteMutation()
+  //const quotes = [
+    // {
+    //   id: 1,
+    //   quoteText: "Don't cry because it's over, smile because it happened.",
+    //   authorName: "Dr. Seuss",
+    //   apocryphal: true,
+    // },
+    // {
+    //   id: 2,
+    //   quoteText: "So many books, so little time.",
+    //   authorName: "Frank Zappa",
+    //   apocryphal: false,
+    // },
+    // {
+    //   id: 3,
+    //   quoteText: "Be yourself; everyone else is already taken.",
+    //   authorName: "Oscar Wilde",
+    //   apocryphal: false,
+    // },
+  //]
   const displayAllQuotes = useSelector(st => st.quotesState.displayAllQuotes)
   const highlightedQuote = useSelector(st => st.quotesState.highlightedQuote)
   const dispatch = useDispatch()
   return (
     <div id="quotes">
-      <h3>Quotes</h3>
+      <h3>Quotes 
+      {quotesLoading || quotesRefreshing && ' loading...'}
+      {quoteToggling && ' switching...'}
+      {quoteDeleting && ' being deleted...'}
+      </h3>
       <div>
         {
           quotes?.filter(qt => {
@@ -45,9 +54,20 @@ export default function Quotes() {
                 <div>{qt.quoteText}</div>
                 <div>{qt.authorName}</div>
                 <div className="quote-buttons">
-                  <button>DELETE</button>
-                  <button onClick={() => dispatch(setHighlightedQuote(qt.id))}>HIGHLIGHT</button>
-                  <button>FAKE</button>
+                  <button
+                  onClick={()=>deleteQuote(
+                    qt.id
+                  )}
+                  >DELETE</button>
+                  <button 
+                  onClick={() => dispatch(
+                    setHighlightedQuote(qt.id))}
+                  >HIGHLIGHT</button>
+                  <button
+                  onClick={()=>toggleFake({
+                    id: qt.id, quote: {apocryphal: !qt.apocryphal}
+                  })}
+                  >FAKE</button>
                 </div>
               </div>
             ))
